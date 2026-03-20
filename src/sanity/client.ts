74f8139849006a2e82987240df102b49a4b003c5
Mohsin Yaqoob <mohsinyqb@gmail.com>
2026-03-20 06:48:28 +0530

    Fixes build error

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -4,12 +4,6 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
 
-if (process.env.NODE_ENV === "production" && (!projectId || !dataset)) {
-  throw new Error(
-    "NEXT_PUBLIC_SANITY_PROJECT_ID and NEXT_PUBLIC_SANITY_DATASET are required in production",
-  );
-}
-
 export const client = createClient({
   projectId: projectId || "abc12345",
   dataset: dataset || "production",
